57e4bd37b3965efed5b4f0193fc6657cf35ee130
rawanlaham <rawan2001allaham@gmail.com>
2024-11-10 23:33:55 +0300

    config

--- app/Http/Controllers/API/TripController.php
+++ app/Http/Controllers/API/TripController.php
@@ -37,11 +37,8 @@ class TripController extends Controller
   {
     try{
     $trip = Trip::findOrFail($id)->getMedia('trip_profile');
-<<<<<<< HEAD
-    return TripResource::collection($trip);
-=======
+    // return TripResource::collection($trip);
     return ImageTripResource::collection($trip);
->>>>>>> 9ec4109fa5cd4da0c440fe96abf38c1ba56f9574
     }
     catch(\Exception $e){
       return response()->json(['error' => 'No Profile image for this trip.'], 500);
